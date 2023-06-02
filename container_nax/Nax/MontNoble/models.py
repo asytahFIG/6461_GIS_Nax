0328d96e1ccd6d1d01c85f9be65f03a4b8224626
benjamin <benjamin.biollaz@students.hevs.ch>
2023-06-02 15:58:41 +0200

    models and database engine

--- container_nax/Nax/MontNoble/models.py
+++ container_nax/Nax/MontNoble/models.py
@@ -1,3 +1,105 @@
 from django.db import models
 
 # Create your models here.
+
+# Ski resort
+
+class SkiSlope(models.Model):
+    id = models.IntegerField
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    obj_type = models.CharField(max_length=100)
+    difficulty = models.IntegerField
+
+class ChairLift(models.Model):
+    id = models.IntegerField
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    obj_type = models.CharField(max_length=100)
+
+# Transport
+
+class Transportation(models.Model):
+    id = models.IntegerField
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    obj_type = models.CharField(max_length=100)
+    dest = models.CharField(max_length=100)
+    available = models.IntegerField
+    altitude =  models.IntegerField
+    subtype = models.CharField(max_length=100)
+
+class Parking(models.Model):
+    id = models.IntegerField
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    obj_type = models.CharField(max_length=100)
+    capacity = models.IntegerField
+    available = models.IntegerField
+
+# Building
+
+class Restaurant(models.Model):
+    id = models.IntegerField
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    obj_type = models.CharField(max_length=100)
+    cost = models.CharField(max_length=100)
+    available = models.IntegerField
+
+class Hotel(models.Model):
+    id = models.IntegerField
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    obj_type = models.CharField(max_length=100)
+    cost = models.CharField(max_length=100)
+    available = models.IntegerField
+    nb_beds = models.IntegerField
+
+class Hut(models.Model):
+    id = models.IntegerField
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    obj_type = models.CharField(max_length=100)
+    altitude = models.IntegerField
+    available = models.IntegerField
+    nb_beds = models.IntegerField
+
+class Facility(models.Model):
+    id = models.IntegerField
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    obj_type = models.CharField(max_length=100)
+    function = models.CharField(max_length=100)
+    available = models.IntegerField
+
+# Nature
+
+class Forest(models.Model):
+    id = models.IntegerField
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    obj_type = models.CharField(max_length=100)
+    protected = models.IntegerField
+
+
+class Summit(models.Model):
+    id = models.IntegerField
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    obj_type = models.CharField(max_length=100)
+    altitude =  models.IntegerField
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- container_nax/Nax/Nax/settings.py
+++ container_nax/Nax/Nax/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Nax.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "admin",
+        "HOST": "db",
+        "PORT": "5432", 
     }
 }
 
