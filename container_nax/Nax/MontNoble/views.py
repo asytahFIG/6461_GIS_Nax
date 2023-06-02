77cff7198be3e39367ba57b7a829a38347581db2
benjamin <benjamin.biollaz@students.hevs.ch>
2023-06-02 16:52:33 +0200

    new migration

--- container_nax/Nax/MontNoble/views.py
+++ container_nax/Nax/MontNoble/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from .models import Forest
 
 def index(request):
-    return HttpResponse("Hello, friend. There you'll find Switzerland.")
+    forests=Forest.objects.filter(type="forest")
+    return HttpResponse(forests[0].name)
 
 # Create your views here.
