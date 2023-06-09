b3a755f4e4f364fbb4562ad2ad953bb71d3b1d12
asytahFIG <stephanie.baechler1@students.hevs.ch>
2023-06-09 14:49:49 +0200

    add all polygon objects on skiDomain

--- container_nax/Nax/MontNoble/urls.py
+++ container_nax/Nax/MontNoble/urls.py
@@ -4,6 +4,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('skiDomain/', views.skiDomain, name='skiDomain'),
     path('forests.json', views.forestsjson, name='forestsjson'),
-    path('skiSlopes.json', views.skiSlopejson, name='skiSlopejson'),
-     path('hotel', views.hotel, name='hotel'),
+    path('skiSlopes.json', views.skiSlopesjson, name='skiSlopesjson'),
+    path('facilities.json', views.facilitiesjson, name='facilitiesjson'),
+    path('hotels.json', views.hotelsjson, name='hotelsjson'),
+    path('restaurants.json', views.restaurantsjson, name='restaurantsjson'),
+    path('parkings.json', views.parkingsjson, name='parkingsjson'),
+    path('huts.json', views.hutsjson, name='hutsjson'),
+    path('hotel', views.hotel, name='hotel'),
 ]
--- container_nax/Nax/MontNoble/views.py
+++ container_nax/Nax/MontNoble/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from .models import Forest, Ski_slope, Hotel
+from .models import Forest, Ski_slope, Hotel, Hut, Facilities, Summit, Chair_lift, Parking, Restaurant, Transportation
 from django.template import loader
 from django.http import Http404
 from django.shortcuts import render
@@ -8,22 +8,56 @@ from django.core.serializers import serialize
 def index(request):
     return render (request, 'MontNoble/index.html')
 
-# Create your views here.
+# Home page with map
 def skiDomain(request):
     context = {}
     return render(request, 'MontNoble/skiDomain.html', context)
 
-# Create your views here.
+# Views for services
 def hotel(request):
-    hteols=Hotel.objects.filter(cost="$$")
-    return HttpResponse(hteols[0].name)
+    hotels=Hotel.objects.filter(cost="$$")
+    return HttpResponse(hotels[0].name)
 
+# Serialized objects to show on map - polygons
 def forestsjson(request):
     forests=Forest.objects.all()
     ser=serialize('geojson', forests, geometry_field='geom', fields=('name', ))
     return HttpResponse(ser, content_type='json')
 
-def skiSlopejson(request):
+def skiSlopesjson(request):
     skiSlopes=Ski_slope.objects.all()
     ser=serialize('geojson', skiSlopes, geometry_field='geom', fields=('name', 'difficulty'))
     return HttpResponse(ser, content_type='json')
+
+def facilitiesjson(request):
+    facilities=Facilities.objects.all()
+    ser=serialize('geojson', facilities, geometry_field='geom', fields=('name', ))
+    return HttpResponse(ser, content_type='json')
+
+def hotelsjson(request):
+    hotels=Hotel.objects.all()
+    ser=serialize('geojson', hotels, geometry_field='geom', fields=('name', ))
+    return HttpResponse(ser, content_type='json')
+
+def restaurantsjson(request):
+    restaurants=Restaurant.objects.all()
+    ser=serialize('geojson', restaurants, geometry_field='geom', fields=('name', ))
+    return HttpResponse(ser, content_type='json')
+
+def parkingsjson(request):
+    parkings=Parking.objects.all()
+    ser=serialize('geojson', parkings, geometry_field='geom', fields=('name', ))
+    return HttpResponse(ser, content_type='json')
+
+def hutsjson(request):
+    huts=Hut.objects.all()
+    ser=serialize('geojson', huts, geometry_field='geom', fields=('name', ))
+    return HttpResponse(ser, content_type='json')
+
+
+# Serialized objects to show on map - points
+
+
+# Serilized objects to show on map - lines
+
+
