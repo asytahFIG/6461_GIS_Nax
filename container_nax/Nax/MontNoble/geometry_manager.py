2ec8ae02e34e6bebee3507b887b1e6a76bb0f5cb
benjamin <benjamin.biollaz@students.hevs.ch>
2023-06-10 13:57:03 +0200

    fixed merge conflicts

---
+++ container_nax/Nax/MontNoble/geometry_manager.py
@@ -0,0 +1,6 @@
+from shapely.geometry import Polygon
+import shapely.wkt
+
+
+def load_shapely_from_geodjango(geoDjangoModel):
+      return shapely.wkt.loads(str(geoDjangoModel.geom).partition(";")[2])
